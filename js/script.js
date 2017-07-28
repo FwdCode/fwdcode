8c6a4f1d082a6b2eadd046af41569298a52f6c2f
Emily Redler <esredler@gmail.com>
2017-07-28 10:52:31 -0400

    please work

--- js/script.js
+++ js/script.js
@@ -36,55 +36,7 @@ $( document ).ready(function() {
  /* Project Gallery   ................... */
  /* ------------------------------------- */
 
-    
-    
-    $(function() {
-
-    function showContent(e, element, navigation, info, img, close) {
-      var eventTarget = e.target.hash;
-      var imgSrc = $(e.target).parents('figure').children('img').attr('src');
-
-      e.preventDefault();
-      $(element).addClass('show');
-
-      $(info).find('li').removeClass('is-visible');
-      $(info).find("li"+eventTarget).addClass('is-visible');
-
-      $(navigation).siblings(".project_info").children('li'+eventTarget).find('img').attr('src', imgSrc);
-    }
-
-    var projectContainerList = document.getElementsByClassName('project');
-
-    $(projectContainerList).each(function() {
-   //  for (var i = 0; i < projectContainerList.length; i++) {
-      var project = $(this);
-      var projectNavigation = $(project).children(".project_navigation");
-      var projectInfo = $(projectNavigation).siblings(".project_info");
-      var projectImg = $('<img>');
-      var closeButton = $('<a href="#close" class="close"><i class="ion-android-close"></i></a>');
-
-      function appendAndPrepend(info, img, close) {
-        var infoTab = $(info).children('li');
-        $(infoTab).append(close);
-        $(infoTab).prepend(img);
-      }  appendAndPrepend(projectInfo, projectImg, closeButton);
-
-      $(projectNavigation).on('click', function(event) {
-        if ( event.target.tagName.toLowerCase() === 'a' ) {
-          showContent(event, project, projectNavigation, projectInfo, projectImg, closeButton);
-        }
-      });
-
-      $('.close').on('click', function(e) {
-        e.preventDefault();
-        $(project).removeClass('show')
-      });
-
-    });
-
-  });
-
-
+  
 /* ------------------------------------- */
 /* Accordion   ................... */
 /* ------------------------------------- */
@@ -152,5 +104,8 @@ for (i = 0; i < acc.length; i++) {
       }
       return false;
     });
+    
+    
+    
 
 });
